1fca151fc1ffb8a69cd372ea9e97a9395425f8df
Mykhailo Pidlisnyi <mpidlisnyi88@gmail.com>
2023-11-12 13:29:16 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -5,15 +5,17 @@ app = QApplication([])
 from main_window import *
 
 
+
+
 def switch_box():
     if btn_next.text() == "Відповісти":
         RadioGroupBox.hide()
-        ...
+        AnswerGroupBox.show()
         btn_next.setText("Наступне питання")
 
-    if btn_next.text() == "Наступне питання":
+    elif btn_next.text() == "Наступне питання":
         RadioGroupBox.show()
-        ...
+        AnswerGroupBox.hide()
         btn_next.setText("Відповісти")
 
 btn_next.clicked.connect(switch_box)
